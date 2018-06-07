aa06438d95488ab5d5cd29579cb19c0dfed0dbed
programarivm <info@programarivm.com>
2018-06-07 19:19:32 +0100

    methods added to the Randomizer class

--- src/Randomizer.php
+++ src/Randomizer.php
@@ -2,8 +2,6 @@
 
 namespace UnicodeRanges;
 
-use UnicodeRanges\Converter;
-
 class Randomizer
 {
     public static function char(array $charRanges)
@@ -13,6 +11,73 @@ class Randomizer
             hexdec($charRanges[$key]->range()[0]),
             hexdec($charRanges[$key]->range()[1])
         );
+
         return Converter::dec2unicode($rand);
+    }
+
+    public static function letter(array $charRanges)
+    {
+        for ($i = 0; $i <= 50; $i++) {
+            $letter = self::char($charRanges);
+            if (preg_match('/\p{L}/u', $letter)) {
+                return $letter;
+            }
+        }
+
+        return false;
+    }
+
+    public static function number(array $charRanges)
+    {
+        for ($i = 0; $i <= 50; $i++) {
+            $number = self::char($charRanges);
+            if (preg_match('/\p{N}/u', $number)) {
+                return $number;
+            }
+        }
+
+        return false;
+    }
+
+    public static function printableChar(array $charRanges)
+    {
+        for ($i = 0; $i <= 50; $i++) {
+            $char = self::char($charRanges);
+            if (preg_match('/(\p{L}|\p{N}|\p{P}|\p{S})/u', $char)) {
+                return $char;
+            }
+        }
+
+        return false;
+    }
+
+    public static function letters(array $charRanges, $length)
+    {
+        $string = '';
+        for ($i = 1; $i <= $length; ++$i) {
+            $string .= self::letter($charRanges);
+        }
+
+        return $string;
+    }
+
+    public static function numbers(array $charRanges, $length)
+    {
+        $string = '';
+        for ($i = 1; $i <= $length; ++$i) {
+            $string .= self::number($charRanges);
+        }
+
+        return $string;
+    }
+
+    public static function printableChars(array $charRanges, $length)
+    {
+        $string = '';
+        for ($i = 1; $i <= $length; ++$i) {
+            $string .= self::printableChar($charRanges);
+        }
+
+        return $string;
     }
 }
---
+++ tests/RandomizerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace UnicodeRanges\Tests;
+
+use PHPUnit\Framework\TestCase;
+use UnicodeRanges\Randomizer;
+use UnicodeRanges\Range\BasicLatin;
+use UnicodeRanges\Range\Tibetan;
+use UnicodeRanges\Range\Cherokee;
+use UnicodeRanges\Range\AegeanNumbers;
+
+class RandomizerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function letter()
+    {
+        $letter = Randomizer::letter([
+            new Cherokee(),
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ]);
+
+        $this->assertEquals(1, mb_strlen($letter));
+        $this->assertEquals(1, preg_match('/\p{L}/u', $letter));
+    }
+
+    /**
+     * @test
+     */
+    public function number()
+    {
+        $number = Randomizer::number([
+            new Cherokee(),
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ]);
+
+        $this->assertEquals(1, mb_strlen($number));
+        $this->assertEquals(1, preg_match('/\p{N}/u', $number));
+    }
+
+    /**
+     * @test
+     */
+    public function printable_char()
+    {
+        $char = Randomizer::printableChar([
+            new Cherokee(),
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ]);
+
+        $this->assertEquals(1, mb_strlen($char));
+        $this->assertEquals(1, preg_match('/(\p{L}|\p{N}|\p{P}|\p{S})/u', $char));
+    }
+
+    /**
+     * @test
+     */
+    public function letters()
+    {
+        $letters = Randomizer::letters([
+            new Cherokee(),
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ], 5);
+
+        $this->assertEquals(5, mb_strlen($letters));
+        $this->assertEquals(1, preg_match('/\p{L}*/u', $letters));
+    }
+
+    /**
+     * @test
+     */
+    public function numbers()
+    {
+        $numbers = Randomizer::letters([
+            new Cherokee(),
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ], 5);
+
+        $this->assertEquals(5, mb_strlen($numbers));
+        $this->assertEquals(1, preg_match('/\p{N}*/u', $numbers));
+    }
+
+    /**
+     * @test
+     */
+    public function printable_chars()
+    {
+        $chars = Randomizer::printableChars([
+            new BasicLatin(),
+            new Tibetan(),
+            new AegeanNumbers(),
+        ], 5);
+
+        $this->assertEquals(5, mb_strlen($chars));
+        $this->assertEquals(1, preg_match('/(\p{L}|\p{N}|\p{P}|\p{S})*/u', $chars));
+    }
+}
